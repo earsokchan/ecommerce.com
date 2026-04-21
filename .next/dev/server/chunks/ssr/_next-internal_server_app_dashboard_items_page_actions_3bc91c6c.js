cdace893314b51d916a71101aa9002dd5f860e61
earsochan <sokchanear0@gmail.com>
2026-04-21 11:14:01 +0700

    fix

--- .next/dev/server/chunks/ssr/_next-internal_server_app_dashboard_items_page_actions_3bc91c6c.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/dashboard/items/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_dashboard_items_page_actions_3bc91c6c.js.map
--- .next/dev/static/chunks/src_app_dashboard_items_page_tsx_ce4cfdc9._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_ee99b078._.js",
-  "static/chunks/node_modules_d3a88027._.js"
-],
-    source: "dynamic"
-});
